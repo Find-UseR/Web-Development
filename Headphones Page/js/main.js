11228e7b2f066bd565d4e92cca6c0fa3efcb589f
AQiB <122538985+Find-UseR@users.noreply.github.com>
2023-03-25 22:44:20 +0500

    Add files via upload

---
+++ Headphones Page/js/main.js
@@ -0,0 +1,8 @@
+let mainImg = document.getElementById("mainImg");
+let container = document.getElementById('container');
+function speakeers(src){
+    mainImg.src = src;
+}
+function backgrounds(color){
+    container.style.background = color;
+}
